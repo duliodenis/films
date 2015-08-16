e1ec94711dad5b8aeffaf81195b43d1bd01ffea3
Dulio Denis <dulio.denis@yahoo.com>
2015-08-16 12:12:41 -0400

    Added a background image blur effect

--- Films/Films/ViewController.swift
+++ Films/Films/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, OMDBAPIControllerDelegate, UISearchBarDe
     
     // Outlets to the View
     @IBOutlet var titleLabel        : UILabel!
+    @IBOutlet var subtitleLabel     : UILabel!
     @IBOutlet var releaseLabel      : UILabel!
     @IBOutlet var ratingLabel       : UILabel!
     @IBOutlet var plotLabel         : UILabel!
@@ -51,11 +52,55 @@ class ViewController: UIViewController, OMDBAPIControllerDelegate, UISearchBarDe
     }
     
     
+    // MARK: - Image Helper Functions
+    
     func imageFromPath(path: String) {
         let posterURL = NSURL(string: path)
         let posterImageData = NSData(contentsOfURL: posterURL!)
         posterImageView.clipsToBounds = true
         posterImageView.image = UIImage(data: posterImageData!)
+        
+        if let imageBlur = posterImageView.image {
+            blurBackgroundImage(imageBlur)
+        }
+    }
+    
+    
+    func blurBackgroundImage(image: UIImage) {
+        // Make a frame the size of the current view
+        var frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+        // Make an image view using that frame
+        var imageView = UIImageView(frame: frame)
+        
+        // Push the received image into the new image view and scale it
+        imageView.image = image
+        imageView.contentMode = .ScaleAspectFill
+        
+        // Create a blur effect view and blur effect
+        var blurEffect = UIBlurEffect(style: .Light)
+        var blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = frame
+        
+        // set up a layer with 30% alpha to dull the blur colors
+        var transparentWhiteView = UIView(frame: frame)
+        transparentWhiteView.backgroundColor = UIColor(white: 1.0, alpha: 0.30)
+        
+        // Add views to our view with an Array
+        var viewsArray = [imageView, blurEffectView, transparentWhiteView]
+        
+        // use a half closed range
+        for index in 0..<viewsArray.count {
+            // remove any old background views with tags
+            if let oldView = view.viewWithTag(index + 1) {
+                oldView.removeFromSuperview()
+            }
+            
+            // insert the view into
+            var viewToInsert = viewsArray[index]
+            view.insertSubview(viewToInsert, atIndex: index + 1)
+            // add the tag in order to remove for the next time
+            viewToInsert.tag = index + 1
+        }
     }
     
     
